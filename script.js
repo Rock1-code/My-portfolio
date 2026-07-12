4eb6fded59f3ee9ba09f3c08ce2fb05c622daca3
Rock1-code <fagyemang390@gmail.com>
2026-07-11 23:57:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+AOS.init({duration:850,once:true});
+new Typed("#typedText",{strings:["Java Backend Developer","Spring Boot Developer","REST API Builder","Full-Stack Web Developer"],typeSpeed:55,backSpeed:35,loop:true});
